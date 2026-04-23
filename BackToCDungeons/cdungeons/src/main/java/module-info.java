a30f6b9945d9306ceb8d616d0409d79c314382de
MarinSucks <marinmjeshtrima@gmail.com>
2026-04-23 23:13:34 +0200

    bug fixing, live reviewing

--- BackToCDungeons/cdungeons/src/main/java/module-info.java
+++ BackToCDungeons/cdungeons/src/main/java/module-info.java
@@ -7,11 +7,13 @@ module com.dungeons {
     requires java.desktop;
 
     opens com.dungeons.Controllers to javafx.fxml;
-    opens com.dungeons.screens to javafx.fxml;        // ← add this
-    opens com.dungeons to javafx.graphics, javafx.fxml; // ← add this
+    opens com.dungeons.screens to javafx.fxml;
+    opens com.dungeons to javafx.graphics, javafx.fxml;
     opens com.dungeons.systems.CombatSystem to javafx.graphics;
+    opens com.dungeons.characters to com.google.gson;
+
     exports com.dungeons to javafx.graphics;
     exports com.dungeons.screens to javafx.graphics, javafx.fxml;
+    exports com.dungeons.systems.CombatSystem to javafx.graphics; // ← add this
     exports com.dungeons.dialogueManager to com.google.gson;
-    opens com.dungeons.characters to com.google.gson;
 }
